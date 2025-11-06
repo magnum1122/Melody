4401ce866c9f2768dc60836a7bdaf936ad1dbc46
abhis <abhishekchauhan4871@gmail.com>
2025-11-06 13:13:13 +0530

    feat: add background color to Loading component for improved visibility

--- components/Loading.tsx
+++ components/Loading.tsx
@@ -3,9 +3,8 @@ import React from 'react'
 
 const Loading = () => {
   return (
-    <View className='flex-1 justify-center items-center'>
+    <View className='flex-1 justify-center items-center bg-ui_950'>
         <ActivityIndicator size={'large'} color={"#C11007"}/>
-      <Text>Loading</Text>
     </View>
   )
 }
